204d9246d0d7c379c71090ed9c0b354a1a9388e6
Om Sharma <omsharma.83173@gmail.com>
2025-05-18 18:39:54 +0530

    trash:removed-existing-stuff.

--- apps/http-backend/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express"
-import userRouter from "../routes/user"
-const router : Router= express.Router();
-
-router.use("/user",userRouter);
-
-
-
-export default router;
--- packages/backend-common/src/index.ts
+++
@@ -1,4 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-export const JWT_SECRET : string | any = process.env.JWT_SECRET;
